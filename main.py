1e7f633cdd82c05ebfb8d558c262b7920c861004
ghaskari <g.askari1037@gmail.com>
2025-01-03 12:25:09 +0330

    edit main to run for configs

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
 import re
-
+import argparse
 from persian_text_preprocessor import PersianTextPreprocessor
 from english_text_preprocessor import EnglishTextPreprocessor
 
@@ -9,8 +9,7 @@ english_file_path = "DataSource/GNOME.en-fa.en"
 persian_file_path = "DataSource/GNOME.en-fa.fa"
 output_directory = "DataSource"
 
-
-def load_text_files(english_path, persian_path):
+def load_text_files(english_path, persian_path, output_path):
     try:
         with open(english_path, 'r', encoding='utf-8') as en_file:
             english_lines = en_file.readlines()
@@ -19,50 +18,40 @@ def load_text_files(english_path, persian_path):
 
         data = {'English': english_lines, 'Persian': persian_lines}
         df = pd.DataFrame(data)
-        print(df)
-
+        df.to_csv(f'{output_path}', index=False)
         return df
     except Exception as e:
         print(f"Error loading text files: {e}")
         return None
 
+def remove_rows_with_only_signs(df, column_name):
+    signs_and_symbols = r'^[^\w\u0600-\u06FF]+$'
+    mask = df.applymap(lambda x: bool(re.match(signs_and_symbols, str(x))) if isinstance(x, str) else False)
+    return df[~mask.any(axis=1)]
+
 def delete_records_with_brackets(df, column_name):
-    try:
-        pattern = re.compile(r'\[.*?\]')
+    pattern = re.compile(r'\[.*?\]')
+    return df[~df[column_name].str.contains(pattern, na=False)].copy()
 
-        cleaned_df = df[~df[column_name].str.contains(pattern, na=False)].copy()
+def process_text_data(df, task):
+    persian_processor = PersianTextPreprocessor(task=task)
+    english_processor = EnglishTextPreprocessor(task=task)
 
-        return cleaned_df
-    except re.error as e:
-        print(f"Regex error: {e}")
-        return df
-
-
-def process_text_data(df):
-
-    persian_processor = PersianTextPreprocessor(task='translation')
-    english_processor = EnglishTextPreprocessor(task='translation')
-
-    # try:
     df['Cleaned_English'] = english_processor.process_column(df['English'])
     df['Cleaned_Persian'] = persian_processor.process_text(df['Persian'])
 
     df = delete_records_with_brackets(df, 'Cleaned_English')
     df = delete_records_with_brackets(df, 'Cleaned_Persian')
-
-    df = df.dropna(how='any', ignore_index=True)
+    df = df.dropna(how='any').reset_index(drop=True)
     df = df[~df.apply(lambda x: x.str.strip().eq('').any(), axis=1)]
 
     df_filtered = df[df['Cleaned_English'] != df['Cleaned_Persian']]
+    df_filtered = remove_rows_with_only_signs(df_filtered, 'Cleaned_English')
+    df_filtered = remove_rows_with_only_signs(df_filtered, 'Cleaned_Persian')
 
     df_final = df_filtered[['Cleaned_English', 'Cleaned_Persian']]
     df_final.columns = ['English', 'Persian']
     return df_final
-
-    # except Exception as e:
-    #     print(f"Error processing text data: {e}")
-    #     return None
-
 
 def save_cleaned_data(df, save_path):
     try:
@@ -71,24 +60,29 @@ def save_cleaned_data(df, save_path):
     except Exception as e:
         print(f"Error saving data: {e}")
 
-
 def main():
-    df = load_text_files(english_file_path, persian_file_path)
+    parser = argparse.ArgumentParser(description="Process translation tasks.")
+    parser.add_argument("--task", type=str, required=True,
+                        choices=["default", "translation", "sentiment", "ner", "topic_modeling", "spam_detection",
+                                 "summarization"],
+                        help="Task configuration to use for processing.")
+    args = parser.parse_args()
+
+    # df = load_text_files(english_file_path, persian_file_path)
+    df = pd.read_csv('DataSource/main_file_gnome.csv')
     if df is None:
         print("Failed to load files. Exiting.")
         return
-    df = df.iloc[30600:,:]
+
     print(df)
 
-    cleaned_df = process_text_data(df)
+    cleaned_df = process_text_data(df, args.task)
     if cleaned_df is None:
-        print("Data processing failed. Exiting.") 
+        print("Data processing failed. Exiting.")
         return
 
-    cleaned_file_path = os.path.join(output_directory, "cleaned_data.csv")
+    cleaned_file_path = os.path.join(output_directory, f"cleaned_data_{args.task}.csv")
     save_cleaned_data(cleaned_df, cleaned_file_path)
-    print(cleaned_df)
-
 
 if __name__ == "__main__":
     main()
